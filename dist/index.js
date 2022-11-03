bbbad5ffb36ac8a047c491af427c06edfb6e2b9a
Taras Petrychkovych <taras.petrychkovych@porsche-engineering.cz>
2022-11-03 10:24:26 +0100

    Parse desc

--- dist/index.js
+++ dist/index.js
@@ -9690,12 +9690,8 @@ const github = __nccwpck_require__(5016);
 try {
   const desc = core.getInput('desc');
   console.log(`Received ${desc}!`);
-  // const time = (new Date()).toTimeString();
-  //core.setOutput("time", time);
-  // Get the JSON webhook payload for the event that triggered the workflow
-  //const payload = JSON.stringify(github.context.payload, undefined, 2)
-  //console.log(`The event payload: ${payload}`);
-  const result = "OK"
+  const result = "NOT OK"
+  var descParts = desc.split('\n');
   core.setOutput("result", result);
 } catch (error) {
   core.setFailed(error.message);
