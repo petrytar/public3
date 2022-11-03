5e21bd8ecf1525d5ee314b1f696ee0c0ee0553d2
Taras Petrychkovych <taras.petrychkovych@porsche-engineering.cz>
2022-11-03 09:46:18 +0100

    Add improves

--- index.js
+++ index.js
@@ -4,11 +4,12 @@ const github = require('@actions/github');
 try {
   const desc = core.getInput('desc');
   console.log(`Received ${desc}!`);
-  const time = (new Date()).toTimeString();
+  //const time = (new Date()).toTimeString();
   //core.setOutput("time", time);
   // Get the JSON webhook payload for the event that triggered the workflow
   //const payload = JSON.stringify(github.context.payload, undefined, 2)
   //console.log(`The event payload: ${payload}`);
+  const result = "OK"
 } catch (error) {
   core.setFailed(error.message);
 }
