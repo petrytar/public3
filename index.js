fb08c3959d4112fa9da0940df0378164f50413c6
Taras Petrychkovych <taras.petrychkovych@porsche-engineering.cz>
2022-11-03 09:20:56 +0100

    Send description

--- index.js
+++ index.js
@@ -2,14 +2,13 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 
 try {
-  // `who-to-greet` input defined in action metadata file
-  const nameToGreet = core.getInput('who-to-greet');
-  console.log(`Hello ${nameToGreet}!`);
+  const desc = core.getInput('desc');
+  console.log(`Received ${desc}!`);
   const time = (new Date()).toTimeString();
-  core.setOutput("time", time);
+  //core.setOutput("time", time);
   // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2)
-  console.log(`The event payload: ${payload}`);
+  //const payload = JSON.stringify(github.context.payload, undefined, 2)
+  //console.log(`The event payload: ${payload}`);
 } catch (error) {
   core.setFailed(error.message);
 }
